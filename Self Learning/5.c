1050f029eb9b09e3dc82ad88e016cddb987ec6f6
Nuno Costa <75766357+Nuno23C@users.noreply.github.com>
2021-02-22 11:43:19 +0000

    Update 5.c

--- Self Learning/5.c
+++ Self Learning/5.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Uma loja de discos anota diariamente durante o mês de março a quantidade de discos vendidos. Determinar em que dia desse mês ocorreu a maior venda e qual foi a quantidade de discos vendida nesse dia.
+// Uma loja de discos anota diariamente durante o mês de março a quantidade de discos vendidos. 
+// Determinar em que dia desse mês ocorreu a maior venda e qual foi a quantidade de discos vendida nesse dia.
 
 void main () {
     int quantidade, dias, maior, i, diaX;
